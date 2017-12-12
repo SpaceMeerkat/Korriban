d03ac2a48a2b61bb07e924ad687b8e5d8302284c
SpaceMeerkat <james.d.astro@gmail.com>
2017-12-12 19:49:57 +0000

    Network connverted into function

--- NN.py
+++ NN.py
@@ -9,47 +9,63 @@ Created on Mon Dec 11 19:28:34 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 #sigmoid function
 def non_linear(x,deriv=False):
         if deriv == True:
                 return x*(1-x)
         return 1./(1+np.exp(-x))
 
-X = np.array([ [0,1,1],[1,1,1],[1,0,1],[0,1,0]])
+def Neural_Network(X,y,iterations,layers):
+        
+        np.random.seed(1)
+        
+        weights_init = np.random.uniform(-1,1,size=(len(X[0]),len(X)))
+        weights_inter = np.random.uniform(-1,1,size=(len(X),len(X),layers+1))
+        weights_final =  np.random.uniform(-1,1,size=(len(X),1))
+        
+        l_inter = np.zeros([layers,len(X),len(X)])
+        l_inter_err = np.zeros([layers,len(X),len(X)])
+        l_inter_delta = np.zeros([layers,len(X),len(X)])
+        
+        for j in range(iterations):      
+                
+                l_init = X    
+                
+                   
+                l_inter[0] = non_linear(np.dot(l_init,weights_init))        
+                for i in range(1,layers):                
+                        l_inter[i] = non_linear(np.dot(l_inter[i-1],weights_inter[i-1]))
+                l_final = non_linear(np.dot(l_inter[-1],weights_final))
+                
+                
+                l_final_err = y - l_final
+                l_final_delta = l_final_err * non_linear(l_final,True)
+                
+                l_inter_err[-1] = l_final_delta.dot(weights_final.T)
+                l_inter_delta[-1] = l_inter_err[-1] * non_linear(l_inter[-1],True)        
+                
+                for k in range(2,layers+1):
+                        
+                        l_inter_err[-k] = l_inter_delta[-(k-1)].dot(weights_inter[-(k-1)].T)
+                        l_inter_delta[-k] = l_inter_err[-k] * non_linear(l_inter[-k],True) 
+                                
+                weights_final += l_inter[-1].T.dot(l_final_delta)
+                
+                for l in range(1,layers):
+                        weights_inter += l_inter[-l].T.dot(l_inter_delta[-l])
+                        
+                weights_init += l_init.T.dot(l_inter_delta[0])
+        
+        return l_final
 
+###############################################################################
+        
+layers = 3
+iterations = 100000
+X = np.array([ [0,1,1],[1,1,1],[1,0,1],[0,1,0]])
 y = np.array([[1,1,0,1]]).T
 
-np.random.seed(1)
+results = Neural_Network(X,y,iterations,layers)
+print(results)
 
-weights1 = np.random.uniform(-1,1,size=(3,4))
-weights2 = np.random.uniform(-1,1,size=(4,1))
-weights3 = np.random.uniform(-1,1,size=(4,1))
-
-err = []
-
-N = 50000
-
-for j in range(N):
-        
-        l0 = X
-        
-        l1 = non_linear(np.dot(l0,weights1))
-        l2 = non_linear(np.dot(l1,weights2))
-        l3 = non_linear(np.dot(l2,weights3))
-                
-        l3_err = y - l3
-              
-        #error weighted derivative        
-        l3_delta = l3_err * non_linear(l3,True)
-        
-        l2_err = l3_delta.dot(weights3.T)        
-        
-        l2_delta = l2_err * non_linear(l2,True)
-        
-        l1_err = l2_delta.dot(weights2.T)
-        
-        l1_delta = l1_err * non_linear(l1,True)
-        
-        weights3 += l2.T.dot(l3_delta)
-        weights2 += l1.T.dot(l2_delta)
-        weights1 += l0.T.dot(l1_delta)
